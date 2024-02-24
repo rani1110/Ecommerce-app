00d239d080150be69bc26aa94437f5f6a90a3525
Rani Kumari <ranikumarijnv1245@gmail.com>
2024-02-24 16:10:29 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data={
+    products:[
+        {
+        _id:'1',
+        name:'Refrigerator',
+        category:'Electronic Items',
+        image:'/images/p4.jpg',
+        price:25000,
+        brand: 'Haier',
+        rating:4.5,
+        numReviews:10,
+        descriptions:'Hight quality product',
+        },
+        {
+            _id:'2',
+            name:'freezer',
+            category:'Electronic Items',
+            image:'/images/p2.jpg',
+            price:29990,
+            brand: 'samsung',
+            rating:4.4,
+            numReviews:10,
+            descriptions:'Hight quality product',
+            },
+
+            {
+                _id:'3',
+                name:'cold-storage box',
+                category:'Electronic Items',
+                image:'/images/p3.jpg',
+                price:15000,
+                brand: 'samsung',
+                rating:4.3,
+                numReviews:10,
+                descriptions:'Hight quality product',
+                },
+
+                {
+                    _id:'4',
+                    name:'freezer',
+                    category:'Electronic Items',
+                    image:'/images/p2.jpg',
+                    price:38990,
+                    brand: 'haier',
+                    rating:4.2,
+                    numReviews:10,
+                    descriptions:'Hight quality product',
+                    },
+
+                    {
+                        _id:'5',
+                        name:'Refrigerator',
+                        category:'Electronic Items',
+                        image:'/images/p4.jpg',
+                        price:24000,
+                        brand: 'samsung',
+                        rating:4.1,
+                        numReviews:10,
+                        descriptions:'Hight quality product',
+                        },
+
+                        {
+                            _id:'6',
+                            name:'cold-storage box',
+                            category:'Electronic Items',
+                            image:'/images/p3.jpg',
+                            price:27990,
+                            brand: 'samsung',
+                            rating:4.0,
+                            numReviews:10,
+                            descriptions:'Hight quality product',
+                            },
+                    
+
+    ],
+};
+export default data;
